9ca48de5a7985775330b3757bd11b026211a756e
m-salguerogaspar <90427703+m-salguerogaspar@users.noreply.github.com>
2021-09-12 10:50:53 -0500

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from time import perf_counter
+from operator import itemgetter
+from tabulate import tabulate
+import contextlib
+import io
+import inspect
+
+
+# In[2]:
+
+
+from task1 import decorator_1
+from task2 import decorator_2
+from task3 import decorator_3
+from task3 import decorator3
+from task4 import decorator_4
+
+
+# In[3]:
+
+
+#@decorator_4
+#@decorator3
+#@decorator_3
+#@decorator_2
+#@decorator_1
+def quadratic_equation_solver(a,b,c):
+    '''
+    Solve a general quadratic equation.
+    
+    param a: Squared Coefficient of x squared
+    param b: Linear Coefficient of x
+    param c: Constant term
+    '''
+    
+    from math import sqrt
+    discriminante = b**2 - 4*(a*c)
+    if discriminante > 0:
+       print(f'Solutions are {-b/(2*a) + sqrt(discriminante)/(2*a)} and {-b/(2*a) - sqrt(discriminante)/(2*a)}')
+    elif discriminante < 0:
+       print(f'Solutions are {-b/(2*a)} + {sqrt(abs(discriminante))/(2*a)} i and {-b/(2*a)} - {sqrt(abs(discriminante))/(2*a)} i')
+    else:
+       print(f'Just one solution {-b/(2*a)}')
+
+
+# In[4]:
+
+
+#@decorator_4
+#@decorator3
+#@decorator_3
+#@decorator_2
+#@decorator_1
+def Fibonacci(number):
+    '''
+    Find the n Fibonacci number.
+    
+    param n: n-th Fibonacci number
+    '''
+    values = [0, 1]
+  
+    any(map(lambda _: values.append(sum(values[-2:])),range(2, number)))
+  
+    print(values[-1])
+
+
+# In[5]:
+
+
+#@decorator_4
+#@decorator3
+#@decorator_3
+#@decorator_2
+#@decorator_1
+def Is_prime(n):
+    '''
+    Find a list of the prime numbers below  n.
+    
+    param n: Upper boundary to find the prime numbers below.
+    '''
+    nums = list()
+    if n == 1:
+       print("No primes")
+    else:
+       nums = range(2,n+1)
+       for i in range(2, n+1):
+           nums = list(filter(lambda x: x == i or x % i, nums))
+       print(nums)
+
+
+# In[6]:
+
+
+#@decorator_4
+#@decorator3
+#@decorator_3
+#@decorator_2
+#@decorator_1
+def Pascal_Triangle(n):
+    '''
+    Find the n-th row of the Pascal Triangle as a list.
+    
+    param n: n-th row of the Pascal Triangle.
+    '''
+    update = [1]
+    y = [0]
+    for x in range(n-1):
+        update=[left+right for left,right in zip(update+y, y+update)]
+    print(update)
+
---
+++ src/task3.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from time import perf_counter
+from operator import itemgetter
+from tabulate import tabulate
+import contextlib
+import io
+import inspect
+
+
+# In[1]:
+
+
+ExeTimeDic = dict()
+
+class decorator_3: 
+        
+     def __init__(self,func):
+        self.func = func
+        self.count = 0
+    
+     
+     def __call__(self,*args,**kwargs):
+        
+        self.count+= 1
+        with contextlib.redirect_stdout(io.StringIO()) as fun:
+            self.start = perf_counter()
+            self.func(*args, **kwargs)
+            self.end = perf_counter()
+            self.time = self.end-self.start
+        self.out = fun.getvalue()
+        global ExeTimeDic
+        ExeTimeDic[self.func.__name__] = self.time
+
+        if len(ExeTimeDic.keys()) == 4:
+           s_f = sorted(ExeTimeDic.values())
+           reverse = dict((v,k) for k,v in ExeTimeDic.items())
+           table = [[reverse[s_f[0]],1,s_f[0]],[reverse[s_f[1]],2,s_f[1]],[reverse[s_f[2]],3,s_f[2]],[reverse[s_f[3]],4,s_f[3]]]
+           print(tabulate(table, headers=["PROGRAM","RANK","TIME ELAPSED"]))
+        else:
+           pass
+        
+        file= open(self.func.__name__ + ".txt","w+")
+        file.write( f'{self.func.__name__} has been called {self.count} times and the execution time was {self.time} \nDESCRIPTION:\nName :{self.func.__name__} \nType: {type(self.func)} \nSign:{inspect.signature(self.func)} \nArgs: positional {args} \nkey=worded {kwargs} \nDoc :{self.func.__doc__} \nSource:{inspect.getsource(self.func)} \nOutput:{self.out}') 
+
+
+# In[2]:
+
+
+ExeTimeDic = dict()                             
+
+def decorator3(func):                             
+    
+    counter = 0
+    
+    def inner_func(*args,**kwargs):  
+                
+        nonlocal counter
+        counter += 1
+        with contextlib.redirect_stdout(io.StringIO()) as fun:
+            start = perf_counter()
+            func(*args,**kwargs)
+            end = perf_counter()
+            time = end-start
+        out = fun.getvalue()
+        
+        global ExeTimeDic
+        ExeTimeDic[func.__name__] = time
+
+        if len(ExeTimeDic.keys()) == 4:
+           s_f = sorted(ExeTimeDic.values())
+           reverse = dict((v,k) for k,v in ExeTimeDic.items())
+           table = [[reverse[s_f[0]],1,s_f[0]],[reverse[s_f[1]],2,s_f[1]],[reverse[s_f[2]],3,s_f[2]],[reverse[s_f[3]],4,s_f[3]]]
+           print(tabulate(table, headers=["PROGRAM","RANK","TIME ELAPSED"]))
+        else:
+           pass
+        
+        file= open(func.__name__ + ".txt","w+")
+        file.write( f'{func.__name__} has been called {counter} times and the execution time was {time} \nDESCRIPTION:\nName :{func.__name__} \nType: {type(func)} \nSign:{inspect.signature(func)} \nArgs: positional {args} \nkey=worded {kwargs} \nDoc :{func.__doc__} \nSource:{inspect.getsource(func)} \nOutput:{out}') 
+                
+    return inner_func
+
